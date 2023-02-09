bf932f3d46d5b16d8d4f1ee8050c34f66022e11e
lisachenko_roman <lisachenkoroma2009@gmail.com>
2023-02-09 15:33:35 +0200

    добавлены img

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from '../Footer/Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={s.footer}>
+            <div>Frontend ReactJS developer</div>
+        </footer>
+    )
+}
+
+export default Footer
